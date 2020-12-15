db8a5975ade54f5d15512af826e3f3caaf1d52b3
Ahmet Buğra Kösen <bugrakosen@windowslive.com>
2020-12-15 23:29:31 +0300

    Added map method overloads.

--- MapperImplementation/MilvasoftMapster/Mapper.cs
+++ MapperImplementation/MilvasoftMapster/Mapper.cs
@@ -1,48 +1,67 @@
 ﻿using Mapster;
 using Milvasoft.MapperImplementation.Implementation;
+using System;
+using System.Linq.Expressions;
 
 namespace Milvasoft.MapperImplementation.MilvasoftMapster
 {
     /// <summary>
-    /// <para><b>EN: </b>Class written instead of manual mapping. It is written to map DTO class object to entity class.</para>
-    /// <para><b>TR: </b>Kaynak nesneden ulaşmak istediğimiz nesneye mapleme işlemini yapmakta sorumlu. DTO sınıfı nesnesini entity sınıfına maplemek için yazılmıştır</para>
+    /// Class written instead of manual mapping. It is written to map DTO class object to entity class.
     /// </summary>
     public class Mapper : IMapper
     {
         /// <summary>
-        /// <para><b>EN: </b>TDestination: The object we want to reach for us, TSource: The object that we will display as the source. For example: I want to map an entity object to a dto object, in this case "TDestination" should be a "dto" object, and "TSource" should be an "entity" object.</para>
-        /// <para><b>TR: </b>TDestination : Bizim için ulaşmak istediğimiz nesne, TSource : Kaynak olarak göstereceğimiz nesne.Örn : Bir entity nesnesini dto nesnesine maplemek istiyorum, bu durumda “TDestination” bir “dto” nesnesi, “TSource” ise bir “entity” nesnesi olmalıdır.</para>
+        /// 
         /// </summary>
+        /// <typeparam name="TSource"></typeparam>
+        /// <typeparam name="TDestination"></typeparam>
+        /// <param name="source"></param>
+        /// <returns></returns>
         public TDestination Map<TSource, TDestination>(TSource source)
         {
-            TypeAdapterConfig<TSource, TDestination>.NewConfig().MaxDepth(2);
+            TypeAdapterConfig<TSource, TDestination>.NewConfig();
             return source.Adapt<TDestination>();
         }
 
-        //MTL MaxDepth metodu ne iş yapıyor en fazla 2 sınıfmı mapleme yapabilsin anlammına geliyor ve Alttaki metot ile bu metotun farkı ne ? 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="TSource"></typeparam>
+        /// <typeparam name="TDestination"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="ignoredMembers"></param>
+        /// <returns></returns>
+        public TDestination Map<TSource, TDestination>(TSource source, Expression<Func<TDestination, object>> ignoredMembers)
+        {
+            TypeAdapterConfig<TSource, TDestination>.NewConfig()
+                        .Ignore(ignoredMembers)
+                        .PreserveReference(true);
+            return source.Adapt<TDestination>();
+        }
 
         /// <summary>
-        /// <para><b>EN: </b>Responsible for mapping from the source object to the object we want to access</para>
-        /// <para><b>TR: </b>Kaynak nesneden ulaşmak istediğimiz nesneye mapleme işlemini yapmakta sorumlu metot</para>
+        /// Responsible for mapping from the source object to the object we want to access.
         /// </summary>
-        public TDestination Map<TDestination>(object source, int maxDepth = 2)
-        { 
-            TypeAdapterConfig<object, TDestination>.NewConfig()
-                      .IgnoreNullValues(true)
+        /// <typeparam name="TSource"></typeparam>
+        /// <typeparam name="TDestination"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="ignoredMembers"></param>
+        /// <param name="maxDepth"></param>
+        /// <param name="ignoreNullValues"></param>
+        /// <returns></returns>
+        public TDestination Map<TSource, TDestination>(TSource source, int maxDepth, bool ignoreNullValues = false, params Expression<Func<TDestination, object>>[] ignoredMembers)
+        {
+            TypeAdapterConfig<TSource, TDestination>.NewConfig()
+                      .Ignore(ignoredMembers)
+                      .IgnoreNullValues(ignoreNullValues)
                       .PreserveReference(true)
                       .MaxDepth(maxDepth);
             return source.Adapt<TDestination>();
-
         }
 
-        //MTL Burada hep 2 degerini alacak (MaxDepth metodu) yukarıdaki metotta parametre olarak alıyor ee ne gerek var bu metoda ? yukarıdaki metot ile aynı işi yapıyor bu metotun olması saçma geldi bana
-        public TDestination Map<TDestination>(object source)
-        {
-            TypeAdapterConfig<object, TDestination>.NewConfig()
-                      .IgnoreNullValues(true)
-                      .PreserveReference(true)
-                      .MaxDepth(2);
-            return source.Adapt<TDestination>();
-        }
+
+
+
+
     }
 }
