bef881be622bf1a183b5bc753c6006017a63f2e6
Ahmet Buğra Kösen <bugrakosen@windowslive.com>
2021-03-29 16:04:35 +0300

    access modifiers added to IMilvaTenantBase.

--- Milvasoft.Helpers/MultiTenancy/EntityBase/IMilvaTenantBase.cs
+++ Milvasoft.Helpers/MultiTenancy/EntityBase/IMilvaTenantBase.cs
@@ -11,24 +11,24 @@ namespace Milvasoft.Helpers.MultiTenancy.EntityBase
         /// <summary>
         /// Display name of the Tenant.
         /// </summary>
-        string TenancyName { get; set; }
+        public string TenancyName { get; set; }
 
         /// <summary>
         /// Display name of the Tenant.
         /// </summary>
-        string Name { get; set; }
+        public string Name { get; set; }
 
         /// <summary>
         /// ENCRYPTED connection string of the tenant database.
         /// Can be null if this tenant is stored in host database.
         /// </summary>
-        string ConnectionString { get; set; }
+        public string ConnectionString { get; set; }
 
         /// <summary>
         /// Is this tenant active?
         /// If as tenant is not active, no user of this tenant can use the application.
         /// </summary>
-        bool IsActive { get; set; }
+        public bool IsActive { get; set; }
     }
 
     /// <summary>
