6aeb56111bb9fedd485f17194ba0917f1a764ed8
Ahmet Buğra Kösen <bugrakosen@windowslive.com>
2021-06-23 23:33:15 +0300

    Added scheduled job operations.

---
+++ Milvasoft.Helpers/JobScheduling/ScheduleConfig.cs
@@ -0,0 +1,49 @@
+﻿using Cronos;
+using System;
+
+namespace Milvasoft.Helpers.JobScheduling
+{
+    /// <summary>
+    /// Schedule config for scheduling jobs.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public interface IScheduleConfig<T>
+    {
+        /// <summary>
+        /// Cron expression of job.
+        /// </summary>
+        string CronExpression { get; set; }
+
+        /// <summary>
+        /// Time zone information of job.
+        /// </summary>
+        TimeZoneInfo TimeZoneInfo { get; set; }
+
+        /// <summary>
+        /// If seconds requested in cron expression send this property.
+        /// </summary>
+        public CronFormat? CronFormat { get; set; }
+    }
+
+    /// <summary>
+    /// Schedule config for scheduling jobs.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class ScheduleConfig<T> : IScheduleConfig<T>
+    {
+        /// <summary>
+        /// Cron expression of job.
+        /// </summary>
+        public string CronExpression { get; set; }
+
+        /// <summary>
+        /// Time zone information of job.
+        /// </summary>
+        public TimeZoneInfo TimeZoneInfo { get; set; }
+
+        /// <summary>
+        /// If seconds requested in cron expression send this property.
+        /// </summary>
+        public CronFormat? CronFormat { get; set; }
+    }
+}
---
+++ Milvasoft.Helpers/JobScheduling/ServiceCollectionExtensions.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Milvasoft.Helpers.Exceptions;
+using System;
+
+namespace Milvasoft.Helpers.JobScheduling
+{
+    /// <summary>
+    /// Provides scheduled job operations.
+    /// </summary>
+    public static class ServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds scheduled cron job to application.
+        /// </summary>
+        /// <typeparam name="TJob"></typeparam>
+        /// <param name="services"></param>
+        /// <param name="options"></param>
+        /// <returns></returns>
+        public static IServiceCollection AddMilvaCronJob<TJob>(this IServiceCollection services, Action<IScheduleConfig<TJob>> options) where TJob : MilvaCronJobService
+        {
+            if (options == null)
+                throw new MilvaDeveloperException("Please provide Schedule Configurations.");
+
+            var config = new ScheduleConfig<TJob>();
+
+            options.Invoke(config);
+
+            if (string.IsNullOrWhiteSpace(config.CronExpression))
+                throw new MilvaDeveloperException("Empty Cron Expression is not allowed.");
+
+            services.AddSingleton<IScheduleConfig<TJob>>(config);
+
+            services.AddHostedService<TJob>();
+
+            return services;
+        }
+    }
+}
