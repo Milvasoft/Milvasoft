6da2340cba3450a19303b40084dca9ee7daa01bc
Ahmet Buğra Kösen <bugrakosen@windowslive.com>
2021-07-03 19:58:21 +0300

    OrderByProps model added. This model applied to mongo base repo methods.

---
+++ Milvasoft.Helpers/Models/OrderByProps.cs
@@ -0,0 +1,18 @@
+﻿namespace Milvasoft.Helpers.Models
+{
+    /// <summary>
+    /// Order by properties for multiple ordey by in database.
+    /// </summary>
+    public class OrderByProps
+    {
+        /// <summary>
+        /// Determines order by Property name of entity.
+        /// </summary>
+        public string PropName { get; set; }
+
+        /// <summary>
+        /// Determines order by ascending or descending.
+        /// </summary>
+        public bool Ascending { get; set; }
+    }
+}
