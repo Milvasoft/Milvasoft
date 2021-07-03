161b2cfbb0a5649ec1a00a14c8ac5066fc470a19
Ahmet Buğra Kösen <bugrakosen@windowslive.com>
2021-07-03 20:00:20 +0300

    ValidateString attribute added.

--- Milvasoft.Helpers/Models/OrderByProps.cs
+++ Milvasoft.Helpers/Models/OrderByProps.cs
@@ -1,4 +1,6 @@
-﻿namespace Milvasoft.Helpers.Models
+﻿using Milvasoft.Helpers.Attributes.Validation;
+
+namespace Milvasoft.Helpers.Models
 {
     /// <summary>
     /// Order by properties for multiple ordey by in database.
@@ -8,6 +10,7 @@
         /// <summary>
         /// Determines order by Property name of entity.
         /// </summary>
+        [ValidateString(1, 100)]
         public string PropName { get; set; }
 
         /// <summary>
