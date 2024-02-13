d2ba0c1736fa4b019a2970f220259985126a4b33
Ahmet Buğra Kösen <bugrakosen@windowslive.com>
2024-02-13 16:18:15 +0300

    api versioning added.
    - routing changed.

---
+++ Milvasoft.Components.Swagger/OperationFilters/ReplaceVersionWithExactValueInPathFilter.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace Milvasoft.Components.Swagger.OperationFilters;
+
+/// <summary>
+/// Replaces version parameter.
+/// </summary>
+public class ReplaceVersionWithExactValueInPathFilter : IDocumentFilter
+{
+    /// <summary>
+    /// Applies configuration.
+    /// </summary>
+    /// <param name="swaggerDoc"></param>
+    /// <param name="context"></param>
+    public void Apply(OpenApiDocument swaggerDoc, DocumentFilterContext context)
+    {
+        var paths = swaggerDoc.Paths;
+        swaggerDoc.Paths = new OpenApiPaths();
+        foreach (var path in paths)
+        {
+            var key = path.Key.Replace("v{version}", swaggerDoc.Info.Version);
+            var value = path.Value;
+            swaggerDoc.Paths.Add(key, value);
+        }
+    }
+}
