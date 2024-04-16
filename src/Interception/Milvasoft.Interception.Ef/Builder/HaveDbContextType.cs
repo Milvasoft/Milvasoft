048dc06c7160097dfba660c8b53ae658ee41d6b3
Ahmet Buğra Kösen <bugrakosen@windowslive.com>
2024-04-16 22:32:32 +0300

    Duplication decreased.

---
+++ src/Interception/Milvasoft.Interception.Ef/Builder/HaveDbContextType.cs
@@ -0,0 +1,52 @@
+﻿namespace Milvasoft.Interception.Ef.Builder;
+
+/// <summary>
+/// Represents the options for no lock interception.
+/// </summary>
+public interface IHaveDbContextType
+{
+    /// <summary>
+    /// DbContext assembly qualified name for configuring options from configuration file.
+    /// For example 'SomeNamespace.SomeContext, SomeNamespace, Version=8.0.0.0, Culture=neutral, PublicKeyToken=null'
+    /// </summary>
+    public string DbContextAssemblyQualifiedName { get; set; }
+
+    /// <summary>
+    /// DbContext type. For example typeof(SomeContext)
+    /// </summary>
+    public Type DbContextType { get; set; }
+
+    /// <summary>
+    /// Returns the dbcontext type to be applied to the transaction.
+    /// </summary>
+    /// <returns>Returns the dbcontext type to be applied to the transaction.</returns>
+    public Type GetDbContextType();
+}
+
+public class HaveDbContextType : IHaveDbContextType
+{
+    protected Type _dbContextType;
+    protected string _dbContextAssemblyQualifiedName;
+
+    public string DbContextAssemblyQualifiedName
+    {
+        get => _dbContextAssemblyQualifiedName;
+        set
+        {
+            _dbContextAssemblyQualifiedName = value;
+            _dbContextType = Type.GetType(_dbContextAssemblyQualifiedName);
+        }
+    }
+
+    public Type DbContextType
+    {
+        get => _dbContextType;
+        set
+        {
+            _dbContextType = value;
+            _dbContextAssemblyQualifiedName = value.AssemblyQualifiedName;
+        }
+    }
+
+    public Type GetDbContextType() => _dbContextType;
+}
--- src/Interception/Milvasoft.Interception.Ef/Transaction/TransactionInterceptionOptions.cs
+++ src/Interception/Milvasoft.Interception.Ef/Transaction/TransactionInterceptionOptions.cs
@@ -1,69 +1,27 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Milvasoft.Core.Abstractions;
 using Milvasoft.Core.Utils.Constants;
+using Milvasoft.Interception.Ef.Builder;
 
 namespace Milvasoft.Interception.Ef.Transaction;
 
 /// <summary>
 /// Represents the options for transaction interception.
 /// </summary>
-[System.Diagnostics.CodeAnalysis.SuppressMessage("Critical Code Smell", "S2696:Instance members should not write to \"static\" fields", Justification = "This value will be assigned only at application startup and acts as a singleton. Therefore, there is no harm in this use.")]
-public class TransactionInterceptionOptions : ITransactionInterceptionOptions
+public class TransactionInterceptionOptions : HaveDbContextType, ITransactionInterceptionOptions
 {
-    private static Type _dbContextType = null;
-    private string _dbContextAssemblyQualifiedName;
-
     public static string SectionName { get; } = $"{MilvaConstant.ParentSectionName}:Interception:Transaction";
 
     public ServiceLifetime InterceptorLifetime { get; set; } = ServiceLifetime.Scoped;
-
-    public string DbContextAssemblyQualifiedName
-    {
-        get => _dbContextAssemblyQualifiedName;
-        set
-        {
-            _dbContextAssemblyQualifiedName = value;
-            _dbContextType = Type.GetType(_dbContextAssemblyQualifiedName);
-        }
-    }
-
-    public Type DbContextType
-    {
-        get => _dbContextType;
-        set
-        {
-            _dbContextType = value;
-            _dbContextAssemblyQualifiedName = value.AssemblyQualifiedName;
-        }
-    }
-
-    public Type GetDbContextType() => _dbContextType ??= Type.GetType(_dbContextAssemblyQualifiedName);
 }
 
 /// <summary>
 /// Represents the options for transaction interception.
 /// </summary>
-public interface ITransactionInterceptionOptions : IMilvaOptions
+public interface ITransactionInterceptionOptions : IHaveDbContextType, IMilvaOptions
 {
     /// <summary>
     /// Transaction interceptor lifetime.
     /// </summary>
     public ServiceLifetime InterceptorLifetime { get; set; }
-
-    /// <summary>
-    /// DbContext assembly qualified name for configuring options from configuration file.
-    /// For example 'SomeNamespace.SomeContext, SomeNamespace, Version=8.0.0.0, Culture=neutral, PublicKeyToken=null'
-    /// </summary>
-    public string DbContextAssemblyQualifiedName { get; set; }
-
-    /// <summary>
-    /// DbContext type. For example typeof(SomeContext)
-    /// </summary>
-    public Type DbContextType { get; set; }
-
-    /// <summary>
-    /// Returns the dbcontext type to be applied to the transaction.
-    /// </summary>
-    /// <returns>Returns the dbcontext type to be applied to the transaction.</returns>
-    public Type GetDbContextType();
 }
