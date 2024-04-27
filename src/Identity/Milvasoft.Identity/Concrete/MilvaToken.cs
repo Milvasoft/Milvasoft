5c81f862f2fdba386715dd14c5fcfe6e5cc8f0de
Ahmet Buğra Kösen <bugrakosen@windowslive.com>
2024-04-27 14:52:07 +0300

    MilvaToken updated.

--- src/Identity/Milvasoft.Identity/Concrete/MilvaToken.cs
+++ src/Identity/Milvasoft.Identity/Concrete/MilvaToken.cs
@@ -1,24 +1,52 @@
 ﻿using Milvasoft.Identity.Abstract;
+using System.Text.Json.Serialization;
 
 namespace Milvasoft.Identity.Concrete;
 
 /// <summary>
-/// Login result DTO for DI.
+/// Token representation.
 /// </summary>
 public record MilvaToken : IToken
 {
     /// <summary>
-    /// If result is success sets the token.
+    /// Gets or sets the access token if the result is successful.
     /// </summary>
+    [JsonPropertyName("access_token")]
     public string AccessToken { get; set; }
 
     /// <summary>
-    /// Expiration data of <see cref="AccessToken"/>.
+    /// Gets or sets the expiration date of the access token.
     /// </summary>
+    [JsonPropertyName("expires_in")]
     public DateTime Expiration { get; set; }
 
     /// <summary>
-    /// Refresh token.
+    /// Gets or sets the expiration date of the refresh token.
     /// </summary>
+    [JsonPropertyName("refresh_expires_in")]
+    public DateTime RefreshTokenExpiration { get; set; }
+
+    /// <summary>
+    /// Gets or sets the refresh token.
+    /// </summary>
+    [JsonPropertyName("refresh_token")]
     public string RefreshToken { get; set; }
+
+    /// <summary>
+    /// Gets or sets the type of the token.
+    /// </summary>
+    [JsonPropertyName("token_type")]
+    public string TokenType { get; set; }
+
+    /// <summary>
+    /// Gets or sets the session state.
+    /// </summary>
+    [JsonPropertyName("session_state")]
+    public string SessionState { get; set; }
+
+    /// <summary>
+    /// Gets or sets the scope of the token.
+    /// </summary>
+    [JsonPropertyName("scope")]
+    public string Scope { get; set; }
 }
