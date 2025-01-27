a86901a0f64d92ae26f2d6c5d7ec223e055c3470
Ahmet Buğra Kösen <bugrakosen@windowslive.com>
2025-01-27 23:11:34 +0300

    fix: Remove unnecessary tenant id assignments in bulk update and delete and remove unnecessary repository methods.

--- src/MultiTenancy/Milvasoft.MultiTenancy.EfCore/RepositoryBase/Concrete/MultiTenantBaseRepository.cs
+++ src/MultiTenancy/Milvasoft.MultiTenancy.EfCore/RepositoryBase/Concrete/MultiTenantBaseRepository.cs
@@ -1,12 +1,10 @@
 ﻿using EFCore.BulkExtensions;
 using Microsoft.EntityFrameworkCore;
-using Milvasoft.Attributes.Annotations;
 using Milvasoft.Core.EntityBases.Abstract;
 using Milvasoft.Core.EntityBases.MultiTenancy;
 using Milvasoft.Core.Helpers;
 using Milvasoft.Core.Utils.Constants;
 using Milvasoft.DataAccess.EfCore.Bulk;
-using Milvasoft.DataAccess.EfCore.Utils.Enums;
 using Milvasoft.Helpers.DataAccess.EfCore.Concrete;
 
 namespace Milvasoft.MultiTenancy.EfCore.RepositoryBase.Concrete;
@@ -41,103 +39,6 @@ public abstract partial class MultiTenantBaseRepository<TEntity, TContext>(TCont
 
             await _dbContext.BulkInsertAsync(entities, bulkConfig, cancellationToken: cancellationToken);
         }
-    }
-
-    /// <summary>
-    /// Bulk update operation. This method will not save changes to the database. So soft delete and auditing operations will not be performed. 
-    /// </summary>
-    /// <param name="entities"></param>
-    /// <param name="bulkConfig"></param>
-    /// <param name="cancellationToken"></param>
-    /// <returns></returns>
-    public override async Task BulkUpdateAsync(List<TEntity> entities, Action<BulkConfig> bulkConfig = null, CancellationToken cancellationToken = default)
-    {
-        if (!entities.IsNullOrEmpty())
-        {
-            SetTenantId(entities);
-
-            if (_dataAccessConfiguration.Auditing.AuditModificationDate)
-                SetPerformTimeValues(entities, EntityPropertyNames.LastModificationDate);
-
-            if (_dataAccessConfiguration.Auditing.AuditModifier)
-                SetPerformerUserValues(entities, EntityPropertyNames.LastModifierUserName);
-
-            await _dbContext.BulkUpdateAsync(entities, bulkConfig, cancellationToken: cancellationToken);
-        }
-    }
-
-    /// <summary>
-    /// Bulk delete operation. This method will not save changes to the database. So soft delete and auditing operations will not be performed. 
-    /// <see cref="CascadeOnDeleteAttribute"/> will not be considered on real delete operations when soft delete is passive.
-    /// </summary>
-    /// <param name="entities"></param>
-    /// <param name="bulkConfig"></param>
-    /// <param name="cancellationToken"></param>
-    /// <returns></returns>
-    public override async Task BulkDeleteAsync(List<TEntity> entities, Action<BulkConfig> bulkConfig = null, CancellationToken cancellationToken = default)
-    {
-        if (!entities.IsNullOrEmpty())
-        {
-            SetTenantId(entities);
-
-            // If soft deletion active and entity is soft deletable.
-            if (_dbContext.GetCurrentSoftDeletionState() == SoftDeletionState.Active && CommonHelper.PropertyExists<TEntity>(EntityPropertyNames.IsDeleted))
-            {
-                //Soft delete
-                SetDeletionValues(entities);
-
-                await BulkUpdateAsync(entities, bulkConfig, cancellationToken);
-
-                return;
-            }
-
-            await _dbContext.BulkDeleteAsync(entities, bulkConfig, cancellationToken: cancellationToken);
-        }
-    }
-
-    /// <summary>
-    /// Bulk add operation. 
-    /// </summary>
-    /// <param name="entities"></param>
-    /// <param name="bulkConfig"></param>
-    /// <param name="cancellationToken"></param>
-    /// <returns></returns>
-    public override async Task BulkAddWithSaveChangesAsync(List<TEntity> entities, BulkConfig bulkConfig = null, CancellationToken cancellationToken = default)
-    {
-        if (!entities.IsNullOrEmpty())
-            _dbSet.AddRange(entities);
-
-        await InternalSaveChangesBulkAsync(bulkConfig, cancellationToken);
-    }
-
-    /// <summary>
-    /// Bulk update operation. 
-    /// </summary>
-    /// <param name="entities"></param>
-    /// <param name="bulkConfig"></param>
-    /// <param name="cancellationToken"></param>
-    /// <returns></returns>
-    public override async Task BulkUpdateWithSaveChangesAsync(List<TEntity> entities, BulkConfig bulkConfig = null, CancellationToken cancellationToken = default)
-    {
-        if (!entities.IsNullOrEmpty())
-            _dbSet.UpdateRange(entities);
-
-        await InternalSaveChangesBulkAsync(bulkConfig, cancellationToken);
-    }
-
-    /// <summary>
-    /// Bulk delete operation. 
-    /// </summary>
-    /// <param name="entities"></param>
-    /// <param name="bulkConfig"></param>
-    /// <param name="cancellationToken"></param>
-    /// <returns></returns>
-    public override async Task BulkDeleteWithSaveChangesAsync(List<TEntity> entities, BulkConfig bulkConfig = null, CancellationToken cancellationToken = default)
-    {
-        if (!entities.IsNullOrEmpty())
-            _dbSet.RemoveRange(entities);
-
-        await InternalSaveChangesBulkAsync(bulkConfig, cancellationToken);
     }
 
     #endregion
--- src/MultiTenancy/Milvasoft.MultiTenancy.EfCore/RepositoryBase/Concrete/SyncMultiTenantBaseRepository.cs
+++ src/MultiTenancy/Milvasoft.MultiTenancy.EfCore/RepositoryBase/Concrete/SyncMultiTenantBaseRepository.cs
@@ -4,7 +4,6 @@ using Milvasoft.Core.EntityBases.Abstract;
 using Milvasoft.Core.Helpers;
 using Milvasoft.Core.Utils.Constants;
 using Milvasoft.DataAccess.EfCore.Bulk;
-using Milvasoft.DataAccess.EfCore.Utils.Enums;
 
 namespace Milvasoft.MultiTenancy.EfCore.RepositoryBase.Concrete;
 
@@ -35,96 +34,6 @@ public abstract partial class MultiTenantBaseRepository<TEntity, TContext> where
 
             _dbContext.BulkInsert(entities, bulkConfig);
         }
-    }
-
-    /// <summary>
-    /// Bulk update operation. 
-    /// </summary>
-    /// <param name="entities"></param>
-    /// <param name="bulkConfig"></param>
-    /// <returns></returns>
-    public override void BulkUpdate(List<TEntity> entities, Action<BulkConfig> bulkConfig = null)
-    {
-        if (!entities.IsNullOrEmpty())
-        {
-            SetTenantId(entities);
-
-            if (_dataAccessConfiguration.Auditing.AuditModificationDate)
-                SetPerformTimeValues(entities, EntityPropertyNames.LastModificationDate);
-
-            if (_dataAccessConfiguration.Auditing.AuditModifier)
-                SetPerformerUserValues(entities, EntityPropertyNames.LastModifierUserName);
-
-            _dbContext.BulkUpdate(entities, bulkConfig);
-        }
-    }
-
-    /// <summary>
-    /// Bulk delete operation. 
-    /// </summary>
-    /// <param name="entities"></param>
-    /// <param name="bulkConfig"></param>
-    /// <returns></returns>
-    public override void BulkDelete(List<TEntity> entities, Action<BulkConfig> bulkConfig = null)
-    {
-        if (!entities.IsNullOrEmpty())
-        {
-            SetTenantId(entities);
-
-            // If soft deletion active and entity is soft deletable.
-            if (_dbContext.GetCurrentSoftDeletionState() == SoftDeletionState.Active && CommonHelper.PropertyExists<TEntity>(EntityPropertyNames.IsDeleted))
-            {
-                //Soft delete
-                SetDeletionValues(entities);
-
-                BulkUpdate(entities, bulkConfig);
-                return;
-            }
-
-            _dbContext.BulkDelete(entities, bulkConfig);
-        }
-    }
-
-    /// <summary>
-    /// Bulk add operation. 
-    /// </summary>
-    /// <param name="entities"></param>
-    /// <param name="bulkConfig"></param>
-    /// <returns></returns>
-    public override void BulkAddWithSaveChanges(List<TEntity> entities, BulkConfig bulkConfig = null)
-    {
-        if (!entities.IsNullOrEmpty())
-            _dbSet.AddRange(entities);
-
-        InternalSaveChangesBulk(bulkConfig);
-    }
-
-    /// <summary>
-    /// Bulk update operation. 
-    /// </summary>
-    /// <param name="entities"></param>
-    /// <param name="bulkConfig"></param>
-    /// <returns></returns>
-    public override void BulkUpdateWithSaveChanges(List<TEntity> entities, BulkConfig bulkConfig = null)
-    {
-        if (!entities.IsNullOrEmpty())
-            _dbSet.UpdateRange(entities);
-
-        InternalSaveChangesBulk(bulkConfig);
-    }
-
-    /// <summary>
-    /// Bulk delete operation. 
-    /// </summary>
-    /// <param name="entities"></param>
-    /// <param name="bulkConfig"></param>
-    /// <returns></returns>
-    public override void BulkDeleteWithSaveChanges(List<TEntity> entities, BulkConfig bulkConfig = null)
-    {
-        if (!entities.IsNullOrEmpty())
-            _dbSet.RemoveRange(entities);
-
-        InternalSaveChangesBulk(bulkConfig);
     }
 
     #endregion
